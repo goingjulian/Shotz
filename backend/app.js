178fccf4072dc5736d6ebfcdf322b0284608c0de
Julian <spam@korfdegidts.nl>
2019-03-31 00:19:22 +0100

    Seperated logic from endpoints. Added error handling

--- backend/app.js
+++ backend/app.js
@@ -7,21 +7,19 @@ import connectMongo from 'connect-mongo'
 import mongoose from 'mongoose'
 import room from './routes/room'
 
-const app = express()
-const httpServer = http.createServer(app);
-
 const MongoStore = connectMongo(session)
 
 const dbName = "shotz"
 
-mongoose.connect(`mongodb://localhost:27017/${dbName}`, { useNewUrlParser: true }, () => {
-    console.log(`DB connected`);
-});
+//Todo:: introduce error handling for when db goes offline after inital startup
+//Todo prevent use of the app when the server is offline
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, { useNewUrlParser: true })
+    .then(
+        () => console.log("DB connected succesfully"),
+        error => console.log(`Error occured while trying to connect to the database, error: ${error}`)
+    )
 
-const webSocketServer = new ws.Server({
-    server: httpServer,
-    path: "/ws"
-})
+const app = express()
 
 app.use(bodyParser.json())
 
@@ -29,12 +27,19 @@ app.use(session({
     resave: true,
     saveUninitialized: false,
     secret: "secretkey",
-    store: new MongoStore({mongooseConnection: mongoose.connection})
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
 
 app.use("/room", room)
 
+// const webSocketServer = new ws.Server({
+//     server: httpServer,
+//     path: "/ws"
+// })
+
 const port = 3000
+
+const httpServer = http.createServer(app);
 
 httpServer.listen(port, () => {
     console.log(`Server listening on port ${port}`)
---
+++ backend/controllers/gameController.js
@@ -0,0 +1,70 @@
+import Game from '../models/game';
+
+//Todo:: introduce error handling for DB ops
+export async function handleRoomCreation(req) {
+    if (!sreq.ession.roomKey) {
+        console.log("Fetching keys")
+        const roomKey = await _generateUniqueRoomKey()
+        console.log("Done fetching keys")
+
+        await _addNewGameToDB(roomKey, req.session.id)
+
+        return roomKey
+    }
+
+    return session.roomKey
+}
+
+//Todo:: introduce error handling for DB ops
+export async function handleRoomJoin(req) {
+    if (!req.body.name) throw new Error("Please provide a name")
+
+    const game = await Game.findOne({ roomKey: req.params.roomKey })
+
+    if (!game) throw new Error("RoomKey is invalid")
+
+    if (game.quizmaster === req.session.id ||
+        game.teams.find(team => team.sessionId === req.session.id)) {
+        throw new Error("You have already joined this quiz as a quizmaster or as a team")
+    }
+
+    game.teams.push({
+        name: req.body.name,
+        sessionId: req.session.id
+    })
+
+    await game.save()
+
+    return req.params.roomKey
+}
+
+async function _addNewGameToDB(roomKey, quizmasterSessionId) {
+
+    const game = new Game({
+        roomKey: roomKey,
+        quizmaster: quizmasterSessionId,
+        teams: []
+
+    })
+    console.log("saving game")
+
+    await game.save()
+    console.log("saved game")
+
+}
+
+async function _generateUniqueRoomKey() {
+    let roomKey = null
+    let isUnique = false
+
+    while (!isUnique) {
+        roomKey = Math.floor(1000 + Math.random() * 9000)
+
+        let keyExists = await Game.findOne({ roomKey: roomKey })
+
+        if (!keyExists) {
+            isUnique = true;
+        }
+    }
+    return roomKey
+}
