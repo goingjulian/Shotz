7443c1814c64cab83ddff8212fefd66b1b2fc165
Wout Jansen <woutj98@gmail.com>
2019-04-02 14:59:27 +0200

    Final refactor back-end

--- backend/src/service/gameService.js
+++ backend/src/service/gameService.js
@@ -2,79 +2,72 @@ import Game from '../models/Game';
 import GameDAO from '../DAO/GameDao';
 import ShotzException from '../exceptions/ShotzException';
 
-const gameDAO = new GameDAO();
-
-// Todo make gameserver class
-// Compact functions
-
-export async function createNewRoom(quizmasterId) {
-    try {
-        const roomCode = await _generateUniqueRoomCode();
-        await gameDAO._addNewGame(roomCode, quizmasterId);
-        return roomCode;
-    }
-    catch(err) {
-        throw new ShotzException('Error occured when trying to create a new game!');
-    }
-}
-
-//Todo:: introduce error handling for DB ops
-export async function handleRoomJoin(req) {
-    if (!req.body.name) throw new Error('Please provide a name');
-
-    const game = await Game.findOne({ roomCode: req.params.roomCode });
-
-    if (!game) throw new Error('Room code is invalid');
-
-    if (
-        game.quizmaster === req.session.id ||
-        game.teams.find(team => team.sessionId === req.session.id)
-    ) {
-        throw new Error(
-            'You have already joined this quiz as a quizmaster or as a team'
-        );
+export default class GameService {
+    constructor() {
+        this.gameDAO = new GameDAO();
     }
 
-    game.teams.push({
-        name: req.body.name,
-        sessionId: req.session.id,
-        accept: false
-    })
-
-    await game.save();
-
-    return req.params.roomCode;
-}
-
-async function _addNewGameToDB(roomCode, quizmasterSessionId) {
-    const game = new Game({
-        roomCode: roomCode,
-        quizmaster: quizmasterSessionId,
-        teams: []
-    });
-    console.log('saving game');
-
-    await game.save();
-    console.log('saved game');
-}
-
-async function _generateUniqueRoomCode() {
-    let roomCode = null;
-    let isUnique = false;
-
-    while (!isUnique) {
-        roomCode = Math.floor(1000 + Math.random() * 9000);
-
-        let keyExists;
+    async createRoom(quizmasterId) {
         try {
-            keyExists = await Game.findOne({ roomCode: roomCode });
+            const roomKey = await this._generateUniqueRoomKey();
+            await this.gameDAO.addNewGame(roomKey, quizmasterId);
+            return roomKey;
         } catch (err) {
-            console.log('ERROR', err);
-        }
-
-        if (!keyExists) {
-            isUnique = true;
+            console.log(err);
+            throw new ShotzException('Error occured when trying to create a new game!');
         }
     }
-    return roomCode;
+
+    async joinRoom(roomKey, sessionId) {
+        const game = await this.gameDAO.getGame(roomKey);
+        if (!game) {
+            throw new ShotzException(`There was no room found with room key ${roomKey}`, 404);
+        } else if (game.gameState !== 'Registration') {
+            throw new ShotzException('Registration is closed for this quiz', 403);
+        } else if (game.quizmaster === sessionId || game.teams.find(team => team.sessionId === sessionId)) {
+            throw new ShotzException('You have already joined this quiz!', 403);
+        } else {
+            await this.gameDAO.joinGameAsTeam(roomKey, sessionId);
+            return `You joined room ${roomKey}`;
+        }
+    }
+
+    async restoreSession(roomKey, sessionId) {
+        const game = await this.gameDAO.getGame(roomKey);
+        if (game.quizmaster === sessionId) {
+            return {
+                role: 'Quizmaster',
+                roomKey: roomKey,
+                gameState: game.gameState,
+                teams: game.teams
+            };
+        } else if (game.teams.find(team => team.sessionId === sessionId)) {
+            return {
+                role: 'Player',
+                roomKey: roomKey,
+                gameState: game.gameState
+            };
+        }
+    }
+
+    async _generateUniqueRoomKey() {
+        let roomKey = null;
+        let isUnique = false;
+
+        while (!isUnique) {
+            roomKey = Math.floor(1000 + Math.random() * 9000);
+
+            let keyExists;
+            try {
+                keyExists = await Game.findOne({ roomKey: roomKey });
+            } catch (err) {
+                console.log('ERROR', err);
+            }
+
+            if (!keyExists) {
+                isUnique = true;
+            }
+        }
+        return roomKey;
+    }
 }
